d219ec6c33b648533f46cac559279eb9828c8a4f
I'm Blue <kotenkovsn@gmail.com>
2022-05-19 17:58:40 +0700

    Зарегистрированы Blueprint для API и закладок

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from main.views import main_blueprint
+from api.views import api_blueprint
+from bookmarks.views import bookmarks_blueprint
 
 
 app = Flask(__name__)
@@ -7,6 +9,8 @@ app.config['JSON_AS_ASCII'] = False
 
 
 app.register_blueprint(main_blueprint)
+app.register_blueprint(api_blueprint)
+app.register_blueprint(bookmarks_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)

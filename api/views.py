8d62d12f3d64741540a55865f2d6906b4a44dad1
I'm Blue <kotenkovsn@gmail.com>
2022-05-19 17:53:13 +0700

    Создан отдельный Blueprint для API

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from flask import jsonify, Blueprint
+from config_path import *
+from utils import LoadJSON
+
+api_blueprint = Blueprint('api_blueprint', __name__, template_folder='templates')
+
+
+
+@api_blueprint.route('/api/posts')
+def api_post():
+    return jsonify(LoadJSON(DATA_PATH).get_posts())
+
+
+@api_blueprint.route('/api/post/<int:pk>')
+def api_post_pk(pk):
+    return jsonify(LoadJSON(DATA_PATH).get_post_by_pk(pk))
